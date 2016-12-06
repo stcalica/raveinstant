419e1319164b574e3cb9a10bea94c0b5b21e0832
kyle calica <kyle.calica-steinhilber@workday.com>
2016-12-05 18:11:42 -0800

    added all files, docker and etc

---
+++ app/public/app.js
@@ -0,0 +1,31 @@
+(function(){
+
+	'user strict';
+
+	var dependencies = [ 'ngRoute' ];
+
+	angular.module('raveinstant', dependencies).config(Config);
+
+	Config.$inject = ['$locationProvider'];
+
+	function Config($locationProvider){
+
+		$locationProvider.hashPrefix('!');
+	}
+
+	if (window.location.hash === '#_=_'){
+		window.location.hash = '#!';
+
+	}
+
+
+	//bootstrap angular
+
+	angular.element(document).ready(function(){
+
+		angular.bootstrap(document, ['raveinstant']);
+
+	});
+	
+
+})();
